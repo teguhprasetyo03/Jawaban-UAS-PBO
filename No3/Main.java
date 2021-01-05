ac31bc1196f9ecd9c0c13ecf01312db2db0d14f3
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2021-01-05 18:39:30 +0700

    ganti no 3

--- No3/Main.java
+++ No3/Main.java
@@ -2,112 +2,288 @@ package UASPBO.No3;
 
 import java.util.Scanner;
 
-abstract class gudang implements pergudangan {
+abstract class Hotel {
+
+    String nama,jenis,tanggal;
+    int harga,lama,total;
+
+    public abstract void setMenu();
+    public abstract void setTanggal();
+    public abstract int setHarga(int harga);
+    public abstract int total(int harga,int lama);
+    public abstract void getData();
+
+
+}
+
+class Pembelian extends Hotel{
+
+
     @Override
-    public abstract void setData();
+    public void setMenu() {
+
+        System.out.println("Pilih jenis kamar ");
+        System.out.println("1. Kamar deluxe Rp.450.000/night");
+        System.out.println("2. Kamar two bedroom Rp.250.000/night");
+    }
+
     @Override
-    public abstract void tampil();
-    public static Scanner in=new Scanner(System.in);
-    public static Scanner str=new Scanner(System.in);
-    public static void main(String[] args) {
-        int menu=0,z=0,a,cari;
-        barang[] data=new barang[1024];
-        data[z]=new barang();
-        data[z].setId();
-        while(menu!=6){
-            System.out.print("menu...\n1.input\t\t2.view\n3.search\t4.update\n5.delete\t6.exit\n : ");
-            menu=data[0].in.nextInt();
-            if(menu==1){
-                z++;
-                data[z]=new barang();
-                data[z].setId(data[z-1].getId());
-                data[z].setData();
-            }else if(menu==2){
-                if(z<1)
-                    System.out.println("data kosong");
-                else{
-                    a=0;
-                    while(a<z){
-                        a++;
-                        System.out.println("---"+a+"---");
-                        data[a].tampil();
+    public void setTanggal() {
+        System.out.println("Tanggal (dd/mm/yyyy) " + this.tanggal);
+    }
+
+    @Override
+    public int setHarga(int harga) {
+        return harga;
+    }
+
+    @Override
+    public int total(int harga, int lama) {
+        return harga * lama;
+    }
+
+    @Override
+    public void getData() {
+        System.out.println("Nama Pelanggan : " + this.nama);
+        System.out.println("Jenis Kamar : " + this.jenis);
+        System.out.println("Tanggal Masuk : " + this.tanggal);
+        System.out.println("Lama Menginap : " + this.lama);
+        System.out.println("Harga Kamar : " + this.harga);
+        System.out.println("Total Pembayaran : " + this.total);
+
+
+    }
+
+
+
+}
+class Pengembalian extends Hotel {
+
+
+    String rekening;
+    @Override
+    public void setMenu() {
+
+        System.out.println("Pilih jenis kamar ");
+        System.out.println("1. Kamar deluxe Rp.450.000/night");
+        System.out.println("2. Kamar two bedroom Rp.250.000/night");
+    }
+
+    @Override
+    public void setTanggal() {
+        System.out.println("Tanggal (dd/mm/yyyy) "+this.tanggal);
+    }
+    @Override
+    public int setHarga(int harga) {
+        return harga;
+    }
+
+    @Override
+    public int total(int harga, int lama) {
+        return harga * lama;
+    }
+
+    @Override
+    public void getData() {
+
+        System.out.println("Nama Pelanggan : " + this.nama);
+        System.out.println("Jenis Kamar : " + this.jenis);
+        System.out.println("Tanggal Menginap : " + this.tanggal);
+        System.out.println("Lama Menginap : " + this.lama);
+        System.out.println("Harga Kamar : " + this.harga);
+        System.out.println("Total Refund : " + this.total);
+        System.out.println("No Rekening : " + this.rekening);
+        System.out.println("Note : Batas penembalian 14 hari kerja " );
+
+
+    }
+}
+
+class Ubah extends Hotel{
+
+
+    @Override
+    public void setMenu() {
+
+        System.out.println("Pilih jenis kamar ");
+        System.out.println("1. Kamar deluxe Rp.450.000/night");
+        System.out.println("2. Kamar two bedroom Rp.250.000/night");
+    }
+
+    @Override
+    public void setTanggal() {
+
+        System.out.println("Tanggal (dd/mm/yyyy) " + this.tanggal);
+    }
+    public String setTanggal(String message) {
+
+        return message;
+    }
+
+    @Override
+    public int setHarga(int harga) {
+        return harga;
+    }
+
+    @Override
+    public int total(int harga, int lama) {
+        return harga * lama;
+    }
+
+    @Override
+    public void getData() {
+
+        System.out.println("Nama Pelanggan : " + this.nama);
+        System.out.println("Jenis Kamar : " + this.jenis);
+        System.out.println("Ganti Tanggal Menginap : " + this.tanggal);
+        System.out.println("Tanggal Menginap : " + this.tanggal);
+        System.out.println("Lama Menginap : " + this.lama);
+        System.out.println("Harga Kamar : " + this.harga);
+        System.out.println("Total Refund : " + this.total);
+        System.out.println("Berhasil di ubah " );
+
+    }
+}
+
+
+
+public class Main {
+
+    public static void main(String [] args){
+
+        Scanner input = new Scanner(System.in);
+        Pembelian pembelian = new Pembelian();
+        Pengembalian pengembalian = new Pengembalian();
+        Ubah ubah = new Ubah();
+
+        boolean a = true;
+
+
+        while (a) {
+            System.out.println("selamat datang di hotel 86");
+            System.out.println("1. Booking");
+            System.out.println("2. Refund");
+            System.out.println("3. Ubah Jadwal");
+            System.out.println("silahkan pilih pilihan anda");
+            int pilih = input.nextInt();
+
+            switch (pilih) {
+                case 1:
+                    System.out.println("Masukan nama anda : ");
+                    pembelian.nama = input.next();
+                    pembelian.setMenu();
+                    System.out.println("silahkan pilih pilihan anda");
+                    int pil1 = input.nextInt();
+
+                    if (pil1 == 1) {
+                        pembelian.jenis = "Deluxe";
+                        pembelian.harga = pembelian.setHarga(450000);
+                    } else if(pil1 == 2){
+                        pembelian.jenis = "Two bedroom";
+                        pembelian.harga = pembelian.setHarga(250000);
+                    }else {
+                        System.out.println("pilihan tidak ada");
+
                     }
-                }
-            }else if(menu==3 || menu==4 || menu==5){
-                System.out.print("masukkan id barang : ");
-                cari=data[0].in.nextInt();
-                a=1;
-                while(a<=z){
-                    if(data[a].getId()==cari)
-                        break;
-                    a++;
-                }
-                if(a<=z){
-                    if(menu==3)
-                        data[a].tampil();
-                    else if(menu==4){
-                        data[a].tampil();
-                        data[a].setData();
+
+                    System.out.print("Masukan tanggal (dd/mm/yyyy): ");
+                    pembelian.tanggal = input.next();
+                    System.out.print("lama menginap : ");
+                    int lama = input.nextInt();
+                    pembelian.lama = lama;
+
+                    System.out.println("total pembayaran : " + pembelian.total(pembelian.harga, lama));
+                    pembelian.total = pembelian.total(pembelian.harga, lama);
+
+                    System.out.println("masukan nominal pembayaran : ");
+                    int nominal = input.nextInt();
+
+                    if (nominal < pembelian.total(pembelian.harga, lama)) {
+
+                        System.out.println("pembayaran gagal");
+
+                    } else {
+
+                        System.out.println("pembayaran berhasil");
                     }
-                    else{
-                        while(a<z){
-                            data[a]=data[a+1];
-                            a++;
-                        }
-                        z--;
-                        System.out.println("data "+cari+" berhasil dihapus");
+
+                    pembelian.getData();
+                    break;
+                case 2:
+
+                    System.out.println("Masukan nama anda : ");
+                    pengembalian.nama = input.next();
+                    pengembalian.setMenu();
+                    System.out.println("silahkan pilih pilihan anda");
+                    int pil2 = input.nextInt();
+                    if (pil2 == 1) {
+                        pengembalian.jenis = "Deluxe";
+                        pengembalian.harga = pengembalian.setHarga(450000);
+                    } else if (pil2 == 2){
+                        pengembalian.jenis = "Two bedroom";
+                        pengembalian.harga = pengembalian.setHarga(250000);
+                    }else {
+                        System.out.println("pilihan tidak ada");
+
                     }
-                }
-                else
-                    System.out.println("data tidak ditemukan");
-            }else if(menu==6)
-                System.out.println("keluar...");
-            else
-                System.out.println("menu ilegal");
+                    a = false;
+                    System.out.print("Masukan tanggal (dd/mm/yyyy): ");
+                    pengembalian.tanggal = input.next();
+                    System.out.print("lama menginap : ");
+                    int lama1 = input.nextInt();
+                    pengembalian.lama = lama1;
+
+                    System.out.println("total pembayaran : " + pengembalian.total(pengembalian.harga, lama1));
+                    pengembalian.total = pengembalian.total(pengembalian.harga, lama1);
+
+                    System.out.println("masukan nomer rekening : ");
+                    String rekening = input.next();
+                    pengembalian.rekening = rekening;
+
+                    if (rekening.isEmpty()) {
+                        System.out.println("Refund Gagal");
+                    } else {
+
+                        System.out.println("Refund Berhasil");
+                    }
+
+                    pengembalian.getData();
+                    break;
+                case 3:
+                    System.out.println("Masukan nama anda : ");
+                    ubah.nama = input.next();
+                    ubah.setMenu();
+                    System.out.println("silahkan pilih pilihan anda");
+                    int pil3 = input.nextInt();
+                    if (pil3 == 1) {
+                        ubah.jenis = "Deluxe";
+                        ubah.harga = ubah.setHarga(450000);
+                    } else if(pil3 == 2){
+                        ubah.jenis = "Two bedroom";
+                        ubah.harga = pengembalian.setHarga(250000);
+                    }else {
+                        System.out.println("pilihan tidak ada");
+                    }
+                    a = false;
+                    System.out.print("Masukan tanggal Menginap(dd/mm/yyyy): ");
+                    ubah.tanggal = input.next();
+                    System.out.print("ganti Tanggal Menginap(dd/mm/yyyy): ");
+                    String tanggal3 = input.next();
+                    ubah.tanggal = ubah.setTanggal(tanggal3);
+
+                    System.out.print("lama menginap : ");
+                    int lama2 = input.nextInt();
+                    ubah.lama = lama2;
+
+                    System.out.println("total pembayaran : " + ubah.total(ubah.harga, lama2));
+                    ubah.total = ubah.total(ubah.harga, lama2);
+
+
+                    ubah.getData();
+
+                default:
+                    System.out.println("pilihan tidak ada");
+            }
         }
-    }
-
-}
-
-interface pergudangan{
-    public int getId();
-    public void setId();
-    public void setId(int id);
-    public void setData();
-    public void tampil();
-}
-
-class barang extends gudang{
-    int idBarang,harga,jumlah,total;
-    String nama;
-    @Override
-    public void setData(){
-        System.out.print("Masukkan Nama Barang  : ");
-        this.nama=str.nextLine();
-        System.out.print("Masukkan Harga Barang : ");
-        this.harga=in.nextInt();
-        System.out.print("Masukkan Jumlah Barang: ");
-        this.jumlah=in.nextInt();
-        this.total=this.harga*this.jumlah;
-    }
-    @Override
-    public void tampil(){
-        System.out.println("Id Barang    : "+this.idBarang);
-        System.out.println("Nama Barang  : "+this.nama);
-        System.out.println("Harga Barang : "+this.harga);
-        System.out.println("Jumlah Barang: "+this.jumlah);
-        System.out.println("Total        : "+this.total);
-    }
-    @Override
-    public int getId(){
-        return this.idBarang;
-    }
-    @Override
-    public void setId(){
-        this.idBarang=153400;
-    }
-    @Override
-    public void setId(int id){
-        this.idBarang=id+1;
     }
 }
